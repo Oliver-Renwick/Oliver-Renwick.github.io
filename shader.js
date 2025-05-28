25100620a2a2be42c9fe668f4231d9444e3996bf
Oliver Renwick <111285385+Oliver-Renwick@users.noreply.github.com>
2025-05-28 18:07:33 +0530

    Update shader.js

--- shader.js
+++ shader.js
@@ -20,7 +20,7 @@ const vertexShaderSource = `
 `;
 
 const fragmentShaderSource = `
- precision mediump float;
+precision mediump float;
 
 uniform float u_time;
 uniform vec2 u_resolution;
@@ -30,16 +30,55 @@ float sphereSDF(vec3 p, vec3 center, float radius) {
     return length(p - center) - radius;
 }
 
+float MandelbulbSDF(vec3 p) {
+    vec3 z = p;
+    float dr = 1.0;
+    float r = 0.0;
+    const int ITERATIONS = 8;
+    const float POWER = 8.0;
+
+    for (int i = 0; i < ITERATIONS; i++) {
+        r = length(z);
+        if (r > 2.0) break;
+
+        // convert to polar coordinates
+        float theta = acos(z.z / r);
+        float phi = atan(z.y, z.x);
+        dr =  pow(r, POWER - 1.0) * POWER * dr + 1.0;
+
+        // scale and rotate the point
+        float zr = pow(r, POWER);
+        theta = theta * POWER;
+        phi = phi * POWER;
+
+        // convert back to cartesian coordinates
+        z = zr * vec3(
+            sin(theta) * cos(phi),
+            sin(phi) * sin(theta),
+            cos(theta)
+        );
+        z += p;
+    }
+    return 0.5 * log(r) * r / dr;
+}
+
+mat3 rotationY(float angle)
+{
+    float c = cos(angle);
+    float s = sin(angle);
+    return mat3(
+        c, 0.0, -s,
+        0.0, 1.0, 0.0,
+        s, 0.0, c
+    );
+}
+
 // Scene SDF that includes animated sphere
 float sceneSDF(vec3 p) {
-    // Move one sphere left and right over time
-    float xOffset = sin(-u_time) * 1.5;
-    vec3 movingCenter = vec3(xOffset, 0.0, 3.5);
+    float angle = u_time * 0.5;
+    p = rotationY(angle) * p;
 
-    float s1 = sphereSDF(p, movingCenter, 1.0);
-    float s2 = sphereSDF(p, vec3(-movingCenter.x,movingCenter.y, -1.5) , 1.0);
-
-    return min(s1, s2);
+    return MandelbulbSDF(p);
 }
 
 // Estimate normal at point
@@ -60,9 +99,11 @@ float getLight(vec3 p) {
     return clamp(dot(n, lightDir), 0.0, 1.0);
 }
 
+
+
 void main() {
     vec2 uv = (gl_FragCoord.xy - 0.5 * u_resolution) / u_resolution.y;
-    vec3 ro = vec3(0.0, 0.0, -5.0); // camera origin
+    vec3 ro = vec3(0.0, 0.0, -4.5); // camera origin
     vec3 rd = normalize(vec3(uv, 1.5)); // ray direction
 
     float t = 0.0;
